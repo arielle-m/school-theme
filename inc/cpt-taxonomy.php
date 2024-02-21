2c0b70a586ad43cf82973ef7130cd9241a66ef12
NandoHsp <fhospina@my.bcit.ca>
2024-02-20 22:11:43 -0800

    Add Staff CPT and Staff Category Taxonomy

--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -2,8 +2,56 @@
 
 // CPTs
 function school_register_custom_post_types() {
-    // Staff CPT
 
+    // Staff CPT
+    $labels = array(
+        'name'               => _x( 'Staff', 'post type general name'  ),
+        'singular_name'      => _x( 'Staff', 'post type singular name'  ),
+        'menu_name'          => _x( 'Staff', 'admin menu'  ),
+        'name_admin_bar'     => _x( 'Staff', 'add new on admin bar' ),
+        'add_new'            => _x( 'Add New', 'staff' ),
+        'add_new_item'       => __( 'Add New Staff' ),
+        'new_item'           => __( 'New Staff' ),
+        'edit_item'          => __( 'Edit Staff' ),
+        'view_item'          => __( 'View Staff'  ),
+        'all_items'          => __( 'All Staff' ),
+        'search_items'       => __( 'Search Staff' ),
+        'parent_item_colon'  => __( 'Parent Staff:' ),
+        'not_found'          => __( 'No staff found.' ),
+        'not_found_in_trash' => __( 'No staff found in Trash.' ),
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'show_in_rest'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'staff' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 30,
+        'menu_icon'          => 'dashicons-groups',
+        'supports'           => array( 'title' ),
+        'template'           => array( 
+                                    array( 'core/paragraph',
+                                        array(
+                                            'placeholder' => 'Add staff biography...'
+                                        ),
+                                    ),
+                                    array( 'core/button',
+                                        array(
+                                            'placeholder' => 'Add staff portfolio...',
+                                        ),
+                                    ),
+                                ),
+        'template_lock'      => 'all',
+    );
+
+    register_post_type( 'school-staff', $args );
 
 
 
@@ -61,12 +109,40 @@ function school_register_custom_post_types() {
 add_action( 'init', 'school_register_custom_post_types' );
 
 
+
 // Taxonomies
 function school_register_taxonomies() {
 
     // Add Staff Category taxonomy
 
+    $labels = array(
+        'name'              => _x( 'Staff Categories', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Staff Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Staff Categories' ),
+        'all_items'         => __( 'All Staff Categories' ),
+        'parent_item'       => __( 'Parent Staff Categories' ),
+        'parent_item_colon' => __( 'Parent Staff Categories:' ),
+        'edit_item'         => __( 'Edit Staff Categories' ),
+        'view_item'         => __( 'View Staff Category' ),
+        'update_item'       => __( 'Update Staff Category' ),
+        'add_new_item'      => __( 'Add New Staff Category' ),
+        'new_item_name'     => __( 'New Work Staff Category' ),
+        'menu_name'         => __( 'Staff Category' ),
+    );
 
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_admin_column' => true,
+        'show_in_rest'      => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'staff-categories' ),
+    );
+
+    register_taxonomy( 'school-staff-category', array( 'school-staff' ), $args );
 
 
     
