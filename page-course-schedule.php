718fa4d7f8be9d50e19981149eba049eccbbacf6
NandoHsp <fhospina@my.bcit.ca>
2024-02-21 16:51:56 -0800

    Delete page-schedule.php, created page-course-schedule.php. Created header Menu - created Student page, created student menu item with submenu.

---
+++ page-course-schedule.php
@@ -0,0 +1,36 @@
+<?php
+
+// https://www.advancedcustomfields.com/resources/repeater/
+
+get_header();
+?>
+
+
+<?php
+get_template_part( 'template-parts/content', 'page' );
+?>
+
+<?php
+if (function_exists ('get_field')) {
+	if (get_field("schedule")) {
+		
+		echo "<table>";
+		echo "<tr>";
+		echo "<th>Date</th>";
+		echo "<th>Course</th>";
+		echo "<th>Instructor</th>";
+		echo "</tr>";
+
+		foreach (get_field("schedule") as $row) {
+			echo "<tr>";
+			echo "<td>" . $row["date"] . "</td>";
+			echo "<td>" . $row["course"] . "</td>";
+			echo "<td>" . $row["instructor"] . "</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+}
+
+
+}
+?>
